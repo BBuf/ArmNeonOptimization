bf94ff4469909ab4929407e830c8600f331bf88d
BBuf <1182563586@qq.com>
2020-08-09 23:43:25 +0800

    Update IN 2020/8/9

--- src/boxFilterBetter.cpp
+++ src/boxFilterBetter.cpp
@@ -1,5 +1,55 @@
 #include "boxFilterBetter.h"
 
 void BoxFilterBetterOrigin(float *Src, float *Dest, int Width, int Height, int Radius){
-    
+    //only 3x3
+    int OutWidth = Width - Radius + 1;
+    int OutHeight = Height - Radius + 1;
+    float *kernel = new float[Radius*Radius];
+    for(int i = 0; i < Radius*Radius; i++){
+        kernel[i] = 1.0;
+    }
+    float *k0 = kernel;
+    float *k1 = kernel + 3;
+    float *k2 = kernel + 6;
+    float* r0 = Src;
+    float* r1 = Src + Width;
+    float* r2 = Src + Width * 2;
+    float* r3 = Src + Width * 3;
+    float* outptr = Dest;
+    float* outptr2 = Dest + OutWidth;
+    int i = 0;
+    for (; i + 1 < OutHeight; i += 2){
+        int remain = OutWidth;
+        for(; remain > 0; remain--){
+            float sum1 = 0, sum2 = 0;
+            sum1 += r0[0] * k0[0];
+            sum1 += r0[1] * k0[1];
+            sum1 += r0[2] * k0[2];
+            sum1 += r1[0] * k1[0];
+            sum1 += r1[1] * k1[1];
+            sum1 += r1[2] * k1[2];
+            sum1 += r2[0] * k2[0];
+            sum1 += r2[1] * k2[1];
+            sum1 += r2[2] * k2[2];
+
+            sum2 += r1[0] * k0[0];
+            sum2 += r1[1] * k0[1];
+            sum2 += r1[2] * k0[2];
+            sum2 += r2[0] * k1[0];
+            sum2 += r2[1] * k1[1];
+            sum2 += r2[2] * k1[2];
+            sum2 += r3[0] * k2[0];
+            sum2 += r3[1] * k2[1];
+            sum2 += r3[2] * k2[2];
+            *outptr = sum1;
+            *outptr2 = sum2;
+            r0++;
+            r1++;
+            r2++;
+            r3++;
+            outptr++;
+            outptr2++;
+        }
+    }
 }
+
--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -5,33 +5,60 @@
 using namespace std;
 using namespace cv;
 
+//测试第一版boxFilter
+// int main(){
+// 	// input image is gray
+// 	Mat src = cv::imread("test_img/car.jpg", 0);
+// 	int Height = src.rows;
+// 	int Width = src.cols;
+// 	int Radius = 3;
+// 	unsigned char *Src = src.data;
+// 	float *Src_float = new float[Height * Width];
+// 	float *Dest = new float[Height * Width];
+
+// 	for(int i = 0; i < Height*Width; i++){
+// 		Src_float[i] = (float)(Src[i]);
+// 	}
+
+// 	vector <float> cache;
+// 	cache.resize(Height * Width);
+
+// 	int64 st = cvGetTickCount();
+// 	for(int i=0; i<10; i++){
+// 		//BoxFilterOrigin(Src_float, Dest, Width, Height, Radius);
+// 		//BoxFilterOpenCV(Src_float, Dest, Width, Height, Radius, cache);
+// 		//BoxFilterOpenCV2(Src_float, Dest, Width, Height, Radius, cache);
+// 		//BoxFilterCache(Src_float, Dest, Width, Height, Radius, cache);
+// 		//BoxFilterNeonAssembly(Src_float, Dest, Width, Height, Radius, cache);
+// 		//BoxFilterNeonAssemblyV1(Src_float, Dest, Width, Height, Radius, cache);
+// 		BoxFilterNeonAssemblyV2(Src_float, Dest, Width, Height, Radius, cache);
+// 		cache.clear();
+// 		printf("Here!\n");
+// 	}
+// 	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 100;
+// 	printf("%.5f\n", duration);
+// 	return 0;
+// }
+
 int main(){
 	// input image is gray
 	Mat src = cv::imread("test_img/car.jpg", 0);
 	int Height = src.rows;
 	int Width = src.cols;
 	int Radius = 3;
+	int OutHeight = Height - Radius + 1;
+	int OutWidth = Width - Radius + 1;
 	unsigned char *Src = src.data;
 	float *Src_float = new float[Height * Width];
-	float *Dest = new float[Height * Width];
+	float *Dest = new float[OutHeight * OutWidth];
 
 	for(int i = 0; i < Height*Width; i++){
 		Src_float[i] = (float)(Src[i]);
 	}
 
-	vector <float> cache;
-	cache.resize(Height * Width);
-
 	int64 st = cvGetTickCount();
 	for(int i=0; i<10; i++){
-		//BoxFilterOrigin(Src_float, Dest, Width, Height, Radius);
-		//BoxFilterOpenCV(Src_float, Dest, Width, Height, Radius, cache);
-		//BoxFilterOpenCV2(Src_float, Dest, Width, Height, Radius, cache);
-		//BoxFilterCache(Src_float, Dest, Width, Height, Radius, cache);
-		//BoxFilterNeonAssembly(Src_float, Dest, Width, Height, Radius, cache);
-		//BoxFilterNeonAssemblyV1(Src_float, Dest, Width, Height, Radius, cache);
-		BoxFilterNeonAssemblyV2(Src_float, Dest, Width, Height, Radius, cache);
-		cache.clear();
+		BoxFilterBetterOrigin(Src_float, Dest, Width, Height, Radius);
 		printf("Here!\n");
 	}
 	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 100;
